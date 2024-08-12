531dde4768637d3fb27ef2666ca406e809211045
Pedro Cardoso de Araujo Lamim <pedro.lamim1812@gmail.com>
2024-08-11 21:54:08 -0300

    comentado

--- calculadora.py
+++ calculadora.py
@@ -9,10 +9,14 @@ while True:
 
     escolha = int(input("Selecione a operação desejada: "));
 
+        # nao ta funcionando direito, quando coloca uma opção inexistente ele so repete o codigo
+        # its not working well, wen u put some wrong number this only repeat the options
+        # mas as opções corretas parecem estar funcionando bem
+        # but the correct answers are working 
     if escolha not in [1, 2, 3, 4, 5]:
         print("Opção invalida.");
         continue
-        
+
     elif escolha == 5:
         print("Encerrando calculadora...")
         break
